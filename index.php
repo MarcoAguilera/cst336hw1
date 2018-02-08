36439c815fb5086fecbe08b6d8f5d24e6a6e492d
Marco Aguilera <maaguilera@csumb.edu>
2018-02-08 06:20:54 +0000

    First Commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title> The Leftovers: Underrated Languages </title>
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <h1 id="border">The Leftovers</h1>
+        </header>
+        <nav class="horzNav">
+            <a id="homeatag"href="index.php">Home</a>
+            <a href="haskell.html">Haskell</a>
+            <a href="lua.html">Lua</a>
+            <a href="scratch.html">Scratch</a>
+            <a href="sources.html">Sources</a>
+        </nav>
+        <main>
+            <div class="even">
+                <img src="img/haskell.png" alt="Picture of Haskell" width=225px height= 225px/>
+                <img src="img/Lua.png" alt="Picture of Lua"  width=225px height= 225px/>
+                <img src="img/scratch.png" alt="Picture of Scratch" width=225px height= 210px/>
+            </div>
+            <div id="homediv">
+                <br />
+                <p>
+                Hello and Welcome To The Leftovers<br />
+                This website is decicated to showcasing different and unusual programming languages.<br/>
+                Today, most people are only aware of the languages that are taught at schools.<br />
+                Whether these decisions by schools are data/statistically driven is yet to be determine.<br />
+                What can't be debated is that there is hundreds of technologies out their that go unused.<br />
+                This website covers three of them, Haskell, Lua, and Scratch.<br />
+                Click the links above to learn more.<br />
+                </p>
+                <em>"Talk is cheap, show me the code" - Linus Torvalds </em>
+            </div>
+        </main>
+        <footer>
+            CST-336. 2018&copy; Aguilera <br />
+            <strong>Disclaimer:</strong> The information in this webpage
+             is fictitous. <br />
+             It is used for academic purposes only.
+             <img id="csumb" src="img/csumb.png" alt="Picture of CSUMB" width=100px height= 85px/>
+        </footer>    
+    </body>
+    
+</html>
